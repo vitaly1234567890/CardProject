21b562b22556a1dc42d1cebe5dcb425ed2ccd3c0
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2024-03-08 12:44:50 +0300

    add components button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,19 +1,29 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import clsx from 'clsx'
 
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
+  children?: ReactNode
+  classname?: string
   fullWidth?: boolean
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    className,
+    fullWidth = false,
+    variant = 'primary',
+    ...rest
+  } = props
 
   return (
     <Component
-      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      className={clsx(s.button, s[variant], fullWidth ? s.fullWidth : '', className)}
       {...rest}
     />
   )
